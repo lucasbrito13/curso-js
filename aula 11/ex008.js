6c99bbdd9e314c851cadc7a5d865cf7628f79746
Lucrixx - Lucas Eduardo <lucaseduardo131922@gmail.com>
2023-09-30 07:03:45 -0300

    Tipos de condições em JS

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velociadae. MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
