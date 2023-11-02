bb450996b67e8280363f765a7e5471fd95c8bcca
Lucrixx - Lucas Eduardo <lucaseduardo131922@gmail.com>
2023-11-02 09:30:23 -0300

    exercicios de integração de dados

---
+++ exercicios/ex001/script.js
@@ -0,0 +1,15 @@
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12){
+    img.src = 'imagens/fotomanha.jpg'
+    document.body.style.background = '#F5B75B'
+}else if(hora >= 12 && hora < 18){
+    img.src = 'imagens/fototarde.jpg'
+    document.body.style.background = '#B79740'
+} else {
+    img.src = 'imagens/fotonoite.jpg'
+    document.body.style.background = '#1F1A16'
+} 
