35cf7a2c06961d85dbc8b649c526a79447b80c4d
Lucrixx - Lucas Eduardo <lucaseduardo131922@gmail.com>
2023-11-06 07:05:15 -0300

    scripts do analisador de  dados

---
+++ exercicios/ex005/js/script.js
@@ -0,0 +1,23 @@
+const number = document.querySelector('#number_entada')
+const lista = document.querySelector('#flista')
+const resultado = document.querySelector('#saida_dados')
+const valores = []
+
+
+function entrada_num(n) {
+
+}
+
+
+function inlista(n, 1) {
+
+}
+
+function adicionar() {
+    if(entrada_num(number.value) && !inlista(number.value, valores)) {
+
+
+    }else {
+        window.alert('Valor invalido ou já encontrado na lista.')
+    }
+}
