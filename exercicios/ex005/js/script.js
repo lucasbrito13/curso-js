a8cdcee2aa16045e916d7a1c67dc38903481370e
Lucrixx - Lucas Eduardo <lucaseduardo131922@gmail.com>
2023-11-07 07:01:51 -0300

    exercicio analizador de dados

--- exercicios/ex005/js/script.js
+++ exercicios/ex005/js/script.js
@@ -1,23 +1,62 @@
-const number = document.querySelector('#number_entada')
+const num = document.querySelector('#number_entada')
 const lista = document.querySelector('#flista')
-const resultado = document.querySelector('#saida_dados')
+const res = document.querySelector('.saida_dados')
 const valores = []
 
 
-function entrada_num(n) {
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        
+        return true;
 
+    } else {
+
+        return false;
+
+    }
 }
 
 
-function inlista(n, 1) {
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        
+        return true;
 
+    } else {
+        
+        return false;
+
+    }
 }
 
 function adicionar() {
-    if(entrada_num(number.value) && !inlista(number.value, valores)) {
+    if(isNumero(num.value) && ! inLista(num.value, valores)) {
 
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
 
     }else {
         window.alert('Valor invalido ou já encontrado na lista.')
+    }
+
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+
+    if(valores.length == 0){
+
+        window.alert('Adicione valores antes de finalizar!')
+
+    } else {
+
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
     }
 }
