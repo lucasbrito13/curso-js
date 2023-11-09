ddf1e22387407bc3f3bad093667d3de6c9c0387b
Lucrixx - Lucas Eduardo <lucaseduardo131922@gmail.com>
2023-11-09 20:13:14 -0300

    site analizador

--- exercicios/ex005/js/script.js
+++ exercicios/ex005/js/script.js
@@ -30,7 +30,7 @@ function inLista(n, l) {
 }
 
 function adicionar() {
-    if(isNumero(num.value) && ! inLista(num.value, valores)) {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
 
         valores.push(Number(num.value))
         let item = document.createElement('option')
@@ -55,8 +55,29 @@ function finalizar() {
     } else {
 
         let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = valores[0]
+        let media = valores[0]
+
+        for(let pos in valores) {
+            
+            soma += valores[pos];
+
+            if(valores[pos] > maior)
+            maior = valores[pos]
+        
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+
+        media = soma / tot
 
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}. </p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}. </p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}. </p>`
+        res.innerHTML += `<p>A media dos valores é ${media} </p>`
     }
 }
